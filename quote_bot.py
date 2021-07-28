137e83a77ba347d2e61e310caf7dbb5ae742cf41
2kai2kai2 <2kai2kai2@gmail.com>
2021-07-27 17:10:56 -0700

    Basic usage of the OpenAI GPT3 API
    Some basic code to utilize GPT3 to generate shower thoughts. There is definitely significant fine-tuning to do for better results and lower cost. The API costs could add up -- just in testing this basic code I've used most of a dollar.
    
    I think the current results fall in the uncanny valley for shower thoughts. Some are just normal, unprofound statements, but they generally aren't bizarre enough to be regularly funny.
    
    Test quotes (first 16):
    'Haircuts take 2 hours in a good city, and 4 to 8 in a bad one.'
    'It’s more likely that you will have diarrhea at some point, than never have diarrhea again.'
    'The most annoying people often say the most truthful things.'
    'If someone asks you “What are you?”, it’s best to just be straight up instead of trying to make them feel better about themselves for their ignorance.'
    
    'The first time you looked up “sex” in a medical dictionary, it was probably hilarious.'
    'If the internet is infinite, then there is somewhere on the internet where I am not banned from posting.'
    "No matter how fast you type, someone else is faster...unless you're sending a text."
    "People who don't get sarcasm are not worth talking to because they have no appreciation for humor." <- true
    
    'When I was a kid my father told me that the opposite of love isn’t hate, it’s indifference. The opposite of life isn’t death, it’s evolution.'
    "You can't learn nothin' from no TV set."
    'Any word that can be mispronounced is mispronounced.'
    "You can't judge a book by its cover, but you can judge a person by their screen name."
    
    'I don’t like going to the movies because when people around me are laughing, I assume they all know something I don’t.'
    'You have to be extremely tall to get a good view of everything at a concert.'
    'Your problems become more solvable if you look at them from 10 000 feet.'
    'The fastest way to lose friends is to ask them for a loan and never pay it back.'

--- quote_bot.py
+++ quote_bot.py
@@ -1,36 +1,117 @@
 # bot that makes showerthoughts
+from typing import List
 import pytumblr
-import random
-import word_lists
-from grammar_checks import *
-from word_lists import lists_noun
-from word_lists import lists_verb
-from word_lists import lists_adj
-from sentences import sentence_structure
+import dotenv
+import openai
 
+# OpenAI
+openai.api_key = dotenv.get_key("keys.env", "OPENAI_KEY")
+
+gpt3prompt = """
+Shower Thoughts
+1. Telling someone to “turn the AC down” can be interpreted as both make it hotter or make it colder.
+2. If you're carrying a pizza, you can get into almost any building.
+3. The olympic pool probably have the lowest pee/water ratio of any public owned pool.
+4. Professors are human programmers. Psychologists are human debuggers.
+5. Being a woman on the internet is half convincing people you’re not a bot and half convincing people you’re not a prostitute.
+6. The smarter you get by reading and experience, the lonlier and crazier you become as you see more stupidity in your midst.
+"""
+
+def generateQuotes(num: int) -> List[str]:
+    # Max num of 4. This is an expensive API.
+    num = max(0, min(num, 4))
+    if num == 0:
+        return []
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=gpt3prompt,
+        temperature=0.75,
+        max_tokens=32 * num,
+        top_p=1,
+        frequency_penalty=0.32,
+        presence_penalty=0.66,
+        stop=[f"{6 + num + 1}."]
+    )
+    data = response["choices"][0]
+    text: str = data["text"]
+    lines = text.splitlines()
+    lines = [line.split(". ", maxsplit=1)[1] for line in lines]
+    # If we didn't get enough text, cut off the end and request the rest as new quotes.
+    if data["finish_reason"] == "length":
+        lines.pop()
+        lines.extend(generateQuotes(num - len(lines)))
+    return lines
+print(generateQuotes(4))
+quit()
 # Authenticate via OAuth
 client = pytumblr.TumblrRestClient(
-    #insert keys here
+    # insert keys here
 )
 
 # Make the request
 client.info()
-# get lists for nouns, adjectives and verbs
 
-trigger_words = ['gun', 'death', 'kill', 'food', 'life', 'dream']
-trigger_warn = ['gun tw', 'death tw', 'death tw', 'food tw', 'unreality tw', 'unreality tw']
+trigger_words = {
+    "gun": "gun tw",
+    "death": "death tw",
+    "kill": "death tw",
+    "food": "food tw",
+    "life": "unreality tw",
+    "dream": "unreality tw"
+}
 
-generate = int(input('how many posts boss?'))
-# Creating a text post
-for i in range(0, generate):
-    taglist = ['bot thoughts', 'shower thoughts', 'bot generated post', 'thoughts from the shower']
-    post = random.choice(sentence_structure).split(' ')
-    for i in range(len(post)):
-        word = post[i]
+# Generate text
+generate = int(input('Number of posts to generate (max 4 at a time): '))
+print("Waiting...")
+quotes = generateQuotes(generate)
+
+# Prepare the text posts
+class stPost:
+    def __init__(self, quote: str, tags: List[str] = []):
+        self.quote = quote
+        self.tags = set(['bot thoughts', 'shower thoughts', 'bot generated post', 'thoughts from the shower'])
+        self.tags.update(tags)
+    def send(self):
+        client.create_text('just-bot-shower-thoughts', state='queue', body=self.quote, tags=self.tags)
+    def __str__(self):
+        return f"'{self.quote}' | {self.tags}"
+posts: List[stPost] = []
+for quote in quotes:
+    post = stPost(quote)
+    for word in quote.split(' '):
         if word in trigger_words:
-            x = trigger_words.index(word)
-            taglist.append(trigger_warn[x])
-    posts = ' '.join(post)
-    client.create_text('just-bot-shower-thoughts', state='queue', body=posts,
-                       tags=taglist)
-
+            post.tags.append(trigger_words[word])
+    # Allow user to modify tags or delete
+    print("\nModify tags and trigger warnings before posting for quote:")
+    print(quote)
+    print(" To continue: READY")
+    print(" To delete post (suggested if very offensive): DELETE")
+    print(" To list existing tags: TAGS")
+    print(" To add/remove tags, just type them and press enter.")
+    while True:
+        command = input("> ").strip()
+        if command == "":
+            continue
+        elif command == "READY":
+            posts.append(post)
+            break
+        elif command == "DELETE":
+            print("Deleted post. It will not be included during upload.")
+            break
+        elif command == "TAGS":
+            print(post.tags)
+        else:
+            if command in post.tags:
+                post.tags.remove(command)
+                print(f"Removed tag '{command}'. New tag list: {post.tags}")
+            else:
+                post.tags.add(command)
+                print(f"Added tag '{command}'. New tag list: {post.tags}")
+print("\nConfirm sending posts:")
+for post in posts:
+    print(post)
+if input("(y/n): ").lower() in ["y", "yes"]:
+    for post in posts:
+        post.send()
+else:
+    print("Modification at this stage has not been implemented. You may manually post quotes if this was a mistake.")
